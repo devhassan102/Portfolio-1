d45d047f79d1322e27a8beb516bcfbc54a9aabe6
devhassan102 <107813182+devhassan102@users.noreply.github.com>
2022-11-17 16:09:54 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import About from './components/About.js';
+import Connect from './components/Connect.js';
+import Navbar from './components/Navbar.js';
+import Projects from './components/Projects.js';
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+    {/* <Router>
+      <Routes>
+
+     
+
+      <Route exact path='About' element={<About name={"Yousaf Raza Gillani"}></About>}></Route>
+
+      <Route exact path='Projects' element={<Projects></Projects>}></Route>
+
+      <Route exact path='Connect' element={<Connect></Connect>}></Route>
+      
+      </Routes>
+      </Router> */}
+      <Navbar  title={"Hassan Gillani"}> </Navbar>
+
+      <About exact path='/About' name={"Hassan Gillani"}> </About>
+      <Projects exact path='/Projects'> </Projects>
+      <Connect exact path='/Connect'> </Connect>
+
+      
+    </>
+  );
+}
+
+export default App;
