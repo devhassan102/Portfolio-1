2bf1cc33b063ac28d3cf3d018bc73bd1d8400af3
devhassan102 <107813182+devhassan102@users.noreply.github.com>
2023-01-10 20:25:13 +0500

    Update Connect.js

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -9,18 +9,18 @@ function Connect() {
         <div className='row-2 row'>
           <div className='row-2-1 col-lg-3 col-md-6'> <p>Write me an email</p><a href='mailto:dev.hassangillani@gmail.com'> <span><i className="fa fa-envelope" aria-hidden="true"></i></span> HASSANGILLANI</a>
           </div>
-          <div className='row-2-2 col-lg-3 col-md-6'> <p>Talk to me</p> <a href='/' > <span> <i className="fa fa-phone" aria-hidden="true"></i> </span> +1 (913) 5866036</a> 
+          <div className='row-2-2 col-lg-3 col-md-6'> <p>Talk to me</p> <a href='/' > <span> <i className="fa fa-phone" aria-hidden="true"></i> </span> </a> 
           </div>
           <div className='row-2-3 col-lg-3 col-md-6'> <img className=' img-size' src='6.png' alt='test' />
           </div>
           <div className='row-2-4 col-lg-3 col-md-6'> <p>Connect with me</p>
-          <a href='/' target='_blank' > <span> <i className="fa fa-linkedin-square" aria-hidden="true"></i> </span> /IN/RANDOMPERSON </a>
+          <a href='/' target='_blank' > <span> <i className="fa fa-linkedin-square" aria-hidden="true"></i> </span> /IN/HASSANGILLANI </a>
           </div>
         </div>
 
       </div>
       <div className='bottom'>
-        <div><p>© The Vice Solution</p></div>
+        <div><p>© Dev It Solution</p></div>
       </div>
     </div>
   )
